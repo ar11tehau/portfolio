efa8722f852dd028f04bedced4603bab5540ce3d
Arii D <108074652+ar11tehau@users.noreply.github.com>
2024-06-13 20:30:24 +0200

    change html CV to pdf CV

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const PORT = 3100
 
 const app = express()
 
+const __dirname = import.meta.dirname;
+
 // Use Static data
 app.use(express.static('public'));
-
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs')
@@ -15,13 +17,18 @@ app.get("/", function(req, res) {
     res.render("index", { title: "Portfolio" })
 })
 
-app.get("/cv/fr", function(req, res) {
-    res.render("cv_fr")
-})
+app.get("/cv", function(req, res) {
+    const filePath = path.join(__dirname, 'public/pdf/DOMELIER_ARIITEHAU_CV.pdf');
+    res.sendFile(filePath)
+});
 
-app.get("/cv/en", function(req, res) {
-    res.render("cv_en")
-})
+// app.get("/cv/fr", function(req, res) {
+//     res.render("cv_fr")
+// })
+
+// app.get("/cv/en", function(req, res) {
+//     res.render("cv_en")
+// })
 
 
 app.listen(PORT, function () {
